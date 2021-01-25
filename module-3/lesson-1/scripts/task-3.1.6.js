27b512ecbf71a2abb899319ad5233bae9b6185f5
keb508 <67642611+keb508@users.noreply.github.com>
2021-01-25 14:14:25 +0000

    Update task-3.1.6.js
    Add variables and concatenate a sentence.
    Use toFixed to get price to two decimal places.

---
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -0,0 +1,6 @@
+let preTipTotal = 45.50;
+let tipPercentage = 17;
+let totalBill = preTipTotal + (preTipTotal * tipPercentage /100);
+document.write("Your total bill, with tip, is £" + totalBill.toFixed(2));
+
+
