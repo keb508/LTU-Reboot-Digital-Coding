b66685256151d18199ec40da099b95ede4a7678f
keb508 <67642611+keb508@users.noreply.github.com>
2021-01-26 11:30:04 +0000

    Update task-3.2.5.js
    Add if and else statemets to compare the temperature variable.

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,24 @@
+var temperature = -9;
+
+if (temperature < 0) {
+    document.write("Stay inside!");
+} else if (temperature < 30) {
+    document.write("Put on a coat and hat!");
+} else if 
+(temperature < 50) {
+    document.write("Put on a coat!");
+} else {
+    document.write("Have fun in the sun!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
