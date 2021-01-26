08a9236a71f3059616bb96fb7cc8f185e13de0eb
keb508 <67642611+keb508@users.noreply.github.com>
2021-01-26 09:44:31 +0000

    Update task-3.1.6.js
    Add tipTotal

--- module-3/lesson-1/scripts/task-3.1.6.js
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -1,6 +1,7 @@
 let preTipTotal = 45.50;
 let tipPercentage = 17;
+let tipTotal = preTipTotal * tipPercentage /100;
 let totalBill = preTipTotal + (preTipTotal * tipPercentage /100);
-document.write("Your total bill, with tip, is £" + totalBill.toFixed(2));
+document.write("Your total bill, with tip, is £" + totalBill.toFixed(2) + ". The tip amount is £" + tipTotal.toFixed(2));
 
 
