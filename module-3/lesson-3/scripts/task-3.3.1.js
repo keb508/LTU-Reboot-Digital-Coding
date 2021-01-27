f083a6eb2597600a22aca7249d107da69352d23e
keb508 <67642611+keb508@users.noreply.github.com>
2021-01-27 10:08:02 +0000

    Update task-3.3.1.js
    Add function percentageCalculator to calulate a percentage.

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,36 @@
+function percentageCalculator(number, percentage) {
+    let answer = (number * percentage)/ 100; 
+        return answer
+}
+
+
+console.log(percentageCalculator(40,20));
+console.log(percentageCalculator(678,3.6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /* 1. Create a function named percentageCalculator
+
+  2. Add 2 arguments for “number” and “percentage”
+
+  3. Do the maths required to work out a percentage with the arguments
+
+  4. Return the result of the maths
+
+  5. Console.log the returned value */
