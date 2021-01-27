8e86270ec675c5d66e04fa6d13dd048e360c8eba
keb508 <67642611+keb508@users.noreply.github.com>
2021-01-27 09:52:07 +0000

    Update task-3.2.6.js
    Add weather program depending on weather and temperature

--- module-3/lesson-2/scripts/task-3.2.6.js
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -1,11 +1,12 @@
-var temperature = -9;
+var temperature = 20;
+var weather = "storm";
 
-if (temperature < 0) {
+if (temperature < 0 || weather == "storm") {
     document.write("Stay inside!");
 } else if (temperature < 30) {
     document.write("Put on a coat and hat!");
 } else if 
-(temperature < 50) {
+(temperature < 50 || weather == "rain") {
     document.write("Put on a coat!");
 } else {
     document.write("Have fun in the sun!");
