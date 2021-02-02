6a1cc4ce52ba90ddbf9c09d6a40fe12f8b3719a6
keb508 <67642611+keb508@users.noreply.github.com>
2021-02-02 10:53:53 +0000

    Create task-3.6.1.js
    Add query selecttor to chabge src attribute.
    Select second paragraph and add styles.

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,5 @@
+let logo = document.querySelector("img");
+    logo.setAttribute("src", "https://images.unsplash.com/photo-1518949396226-d2cd9ab6889e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHw%3D&auto=format&fit=crop&w=500&q=60");
+
+let secondParagraph = document.querySelectorAll('p')[1];
+    secondParagraph.style.border = '4px solid goldenrod';
