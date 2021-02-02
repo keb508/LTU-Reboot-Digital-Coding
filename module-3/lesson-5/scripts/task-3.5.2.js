939d377d0817373ea0a614e14cdb17fa22e649e1
keb508 <67642611+keb508@users.noreply.github.com>
2021-02-02 09:31:33 +0000

    Update task-3.5.2.js
    Changeing the value you can put in for the discount

--- module-3/lesson-5/scripts/task-3.5.2.js
+++ module-3/lesson-5/scripts/task-3.5.2.js
@@ -1,9 +1,9 @@
 function foodDiscount(arr) {
     var totalPrice = 0;
     for (let i = 0; i < arr.length; i++) {
-        let discount = (arr[i].type === 'food' ? 0.2 : 0) 
+        let discount = (arr[i].type === 'food' ? 20 : 0) 
         
-        totalPrice += (arr[i].price * arr[i].quantity) * (1 - discount);
+        totalPrice += (arr[i].price * arr[i].quantity) * (1 - discount/100);
     }
     console.log('£' + totalPrice.toFixed(2));
 }
