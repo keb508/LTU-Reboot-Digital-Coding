af77f990dbafcc1d4846b17cbc330f86c3e42fe1
keb508 <67642611+keb508@users.noreply.github.com>
2021-02-04 11:37:44 +0000

    Update task-3.7.1.js
    create filterSearch and filterButton to be able to use the search bar.
    create function to remove active from buttons to show when a dataclass is selected by the user.

--- module-3/lesson-7/scripts/task-3.7.1.js
+++ module-3/lesson-7/scripts/task-3.7.1.js
@@ -2,6 +2,12 @@ const buttons = document.querySelectorAll('button.selection');
 
 const allimages = document.querySelectorAll('main .image-selections .images');
 
+let filterSearch = document.querySelector('input[name="filter"]');
+
+let filterButton = document.querySelector('button.filter-button');
+
+
+// toggleimages function
 function toggleimages( dataClass ) {
     if(dataClass === 'all') {
 
@@ -32,19 +38,68 @@ function toggleimages( dataClass ) {
 
 
 
-    }
+    } //end of if else
 
 
+} //end of toggleimages
+
+//toggleActiveClass
+
+function toggleActiveClass(active) {
+
+buttons.forEach(function(button) {
+
+button.classList.remove('active');
+
+});
+
+active.classList.add('active');
+
+} //end of toggleActiveClass
+
+            //filter button
+
+    filterButton.addEventListener('click', function() {
+
+        let inputValue = filterSearch.value;
+
+        let searchTerm = inputValue.toLowerCase();
+
+        toggleimages(searchTerm);
+
+        buttons.forEach(function(button) {
+
+        let dataClass = button.getAttribute('data-class');
+
+            if(dataClass === searchTerm) {
+
+                button.classList.add('active');
+
+            } else {
+
+                button.classList.remove('active');
+
+            } //end of if else
+
+        filterSearch.value = '';
 
 
-}
+    }); //end of forEach loop
+
+
+});
+
+//end of filter button
+
 
 buttons.forEach(function(button){
 
 button.addEventListener('click', function(){
 
     toggleimages(button.dataset.class);
+
+    toggleActiveClass(button);
 });
 
-});
+}); //end of if else
 
