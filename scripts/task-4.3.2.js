42a42b9617dc76fa2d85ed18d673ee52387f4cb2
keb508 <67642611+keb508@users.noreply.github.com>
2021-02-09 13:30:24 +0000

    Create task-4.3.2.js
    trying to write a function to replace any letter that is the same as the first letter in the word with a *

---
+++ scripts/task-4.3.2.js
@@ -0,0 +1,24 @@
+// fixStart
+
+/*
+Create a function that will take a string and will replace any letters that are the same as the first letter with a *
+*/
+
+// 1. Identify the first letter of the string
+// 2. Loop through the string and identify if any of the other letters are the same.
+// 3. If the letters are the same replace the letter with a *
+// 4. Return new string.
+
+
+function fixStart(word) {
+    let first = word.charAt(0);
+
+    var newWord = word.replace(/first/g, "*");
+      
+    console.log(newWord);
+ 
+
+    }
+
+
+fixStart('babble');
